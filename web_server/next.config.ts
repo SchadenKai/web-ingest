aeac5d2a48008f39c1dcad69a6287b13dc6ef6aa
SchadenKai <kairustecsonpersonal@gmail.com>
2024-12-29 10:29:32 +0800

    feat: Add API_SERVER environment variable to Next.js configuration

--- web_server/next.config.ts
+++ web_server/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_SERVER: process.env.API_SERVER,
+  },
 };
 
 export default nextConfig;
